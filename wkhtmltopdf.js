35fedbf88d44f3d10d6aec21f40811787a4af08d
Batist Leman <batist.leman@gmail.com>
2014-06-23 22:46:18 +0200

    testing things

--- wkhtmltopdf.js
+++ wkhtmltopdf.js
@@ -1,1 +1,7 @@
-wkhtmltopdf = Npm.require('wkhtmltopdf');
+var wkhtmltopdf;
+
+wkhtmltopdf.prototype.render = Npm.require('wkhtmltopdf');
+
+wkhtmltopdf.prototype.test = function(){
+	console.log("testing things");
+};
